9fdf794367568df5377db5d7ae18b14df0490288
Nikolai <borodinov.nikolai@gmail.com>
2023-11-01 00:41:29 +0300

    commit

--- src/app/catalog/[product]/page.tsx
+++ src/app/catalog/[product]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import React from 'react'
 import styles from './product.module.scss'
 import Link from 'next/link'
+import { Button } from 'antd'
 
 
 const Product = async ({params}:{params:{product:string}}) => {
@@ -14,19 +15,31 @@ const Product = async ({params}:{params:{product:string}}) => {
 
 
 
-
+ if(productInfo){
   return (
     <div className={styles.root}>
-      <button><Link href={product.attributes.buyURL} >Купить</Link></button>
-     {
-      productInfo?.product?.preview_imgs?.slice(1).map( async ({id, url}:{id: number,url:string})=>{
+      <h1 className={styles.name} >{product?.attributes.name}</h1>
+      <div className={styles.buyBtn} ><Button size='large' type='primary' ><Link href={product.attributes.buyURL} >Купить</Link></Button></div>
     
-        return <Image key={id} src={url} height={1200} width={1200} alt='preview'></Image>
-      })
-     }
-     <div dangerouslySetInnerHTML={{ __html: productInfo.product.info }} />
+      
+         <div className={styles.gallery}>
+<></>
+           {
+             productInfo?.product?.preview_imgs && productInfo?.product?.preview_imgs?.slice(1).map( async ({id, url}:{id: number,url:string})=>{
+              return (<div className={styles.image_holder}>
+                <Image key={id} src={url} height={200} width={400} quality={70} alt='preview'></Image>
+              </div>)
+            })
+           }
+
+         </div>
+    
+    
+     <div className={styles.info} dangerouslySetInnerHTML={{ __html: productInfo.product.info }} />
     </div>
   )
+ }
+  
 }
 
 export default Product
