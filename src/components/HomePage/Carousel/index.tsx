2c4c1526bab8cc45af04b5a68918cea9ddeea060
borodinov_nikolai <borodinov.nikolai@gmail.com>
2023-11-07 21:11:32 +0300

    commit

--- src/components/HomePage/Carousel/index.tsx
+++ src/components/HomePage/Carousel/index.tsx
@@ -4,6 +4,7 @@ import { getCarousel } from '@/http/serverApi';
 import Image from 'next/image';
 import styles from './carousel.module.scss'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import Link from 'next/link';
 
 
 const contentStyle: React.CSSProperties = {
@@ -22,7 +23,7 @@ const Carousel_ = async () => {
   const carousel = await getCarousel(1);
   const carousel2 = await getCarousel(2);
     const image = carousel?.data.attributes.image.data[0].attributes.url
-    const image2 = carousel2?.data.attributes.image.data[0].attributes.url
+    const mobile_image = carousel?.data.attributes.image.data[1].attributes.url
 
   console.log(carousel?.data.attributes?.image.data[0].attributes.url)
 
@@ -30,29 +31,32 @@ const Carousel_ = async () => {
   const right = <div>right</div>
 
   return (
-    <Carousel fade={true} prevArrow={<LeftOutlined/>} nextArrow={<RightOutlined/>} arrows={true} autoplay={true}  className={styles.root} >
+    <Carousel prevArrow={<LeftOutlined />} nextArrow={<RightOutlined/>}  fade={true} arrows={true} autoplay={true}  className={styles.root} >
       <div className={styles.item} >
+        <Link href='/catalog'>
         <div className={styles.image} >
-            <Image src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
+            <Image className={styles.img_desktop} src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
+            <Image className={styles.img_mobile} src={process.env.NEXT_PUBLIC_CMS_IMG_URL+mobile_image} width={4000} height={4000} alt='spider man 2'></Image>
         </div>      
      
-       <div className={styles.text} > Spider-Man 2</div>
+       <div className={styles.info} >
+        <div className={styles.info_name}>Spider-Man 2</div>
+        <div className={styles.info_price} > 4000 <span>₽</span>  </div>
+         </div>
+       </Link>
       </div>
-      <div className={styles.item}>
-      <div className={styles.image} >
-            <Image src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
+      <div className={styles.item} >
+        <div className={styles.image} >
+            <Image className={styles.img_desktop} src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
+            <Image className={styles.img_mobile} src={process.env.NEXT_PUBLIC_CMS_IMG_URL+mobile_image} width={4000} height={4000} alt='spider man 2'></Image>
         </div>      
+     
+        <div className={styles.info} >
+        <div className={styles.info_name}>Spider-Man 2</div>
+        <div className={styles.info_price} > 4000 <span>₽</span></div>
+         </div>
       </div>
-      <div className={styles.item}>
-      <div className={styles.image} >
-            <Image src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
-        </div>      
-      </div>
-      <div className={styles.item}>
-      <div className={styles.image} >
-            <Image src={process.env.NEXT_PUBLIC_CMS_IMG_URL+image} width={4000} height={4000} alt='spider man 2'></Image>
-        </div>      
-      </div>
+
     </Carousel>
   );
 };
